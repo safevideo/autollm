b15b9f0f15a583eeb8856b954546c61c6a20eb1b
SeeknnDestroy <sarit17@itu.edu.tr>
2023-09-21 19:19:39 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,34 @@
+from llama_index import VectorStoreIndex, download_loader
+from pathlib import Path
+from multi_markdown_reader import MultiMarkdownReader
+
+# Initialize the MultiMarkdownReader
+markdown_reader = MultiMarkdownReader()
+
+# Folder containing the markdown files
+folder_path = Path('./data')
+
+# Load the data
+documents = markdown_reader.load_data_from_folder(folder_path)
+
+# Print the number of documents loaded
+print(f"Number of 'header-documents' loaded: {len(documents)}")
+
+# Print the first document as a sample
+if documents:
+    print("Sample 'header-document':")
+    print(documents[0].text)
+
+
+# Create an index from the documents
+index = VectorStoreIndex.from_documents(documents)
+
+# Create a query engine
+query_engine = index.as_query_engine()
+
+# Sample user query
+user_query = "Tell me about authors favourite food and exercise."
+
+# Get response
+response = query_engine.query(user_query)
+print(response)
