91e2bbceb521d5a4c2787a0e53541ed163ac2073
SeeknnDestroy <sarit17@itu.edu.tr>
2023-09-29 17:36:31 +0300

    Add update_index_for_changed_files

--- llama_utils.py
+++ llama_utils.py
@@ -1,7 +1,9 @@
-from llama_index.vector_stores.types import ExactMatchFilter, MetadataFilters
-from llama_index import VectorStoreIndex
 from pathlib import Path
 from typing import List
+
+from llama_index.vector_stores.types import ExactMatchFilter, MetadataFilters
+from llama_index import VectorStoreIndex, Document
+
 
 def delete_docs_from_changed_files(index: VectorStoreIndex, changed_files: List[Path]):
     """
@@ -20,3 +22,24 @@ def delete_docs_from_changed_files(index: VectorStoreIndex, changed_files: List[
         
         for doc in docs_to_delete:
             index.delete_ref_doc(doc.node.id_, delete_from_docstore=True)
+
+
+def update_index_for_changed_files(index: VectorStoreIndex, changed_files: List[Path], markdown_reader) -> None:
+    """
+    Delete old documents and insert new ones for the changed files.
+
+    Parameters:
+        index (VectorStoreIndex): The current Llama VectorStoreIndex.
+        changed_files (List[Path]): List of changed markdown files.
+        markdown_reader: An instance of MarkdownReader class.
+    """
+    # Delete old header-docs for changed files
+    delete_docs_from_changed_files(index, changed_files)
+
+    # Process the updated markdown files and insert new header-docs
+    for file in changed_files:
+        extra_info = {"original_file_path": str(file)}
+        new_documents = markdown_reader.load_data(file, extra_info=extra_info)
+
+        for doc in new_documents:
+            index.insert(Document(text=doc.text, metadata=doc.metadata))
--- update_vector_store.py
+++ update_vector_store.py
@@ -5,11 +5,11 @@ import os
 
 from git_utils import clone_or_pull_repository
 from hash_utils import check_for_changes
-from llama_utils import delete_docs_from_changed_files
+from llama_utils import delete_docs_from_changed_files, update_index_for_changed_files
 from markdown_reader import MarkdownReader
 from markdown_processing import process_markdown_files, get_markdown_files
 
-from llama_index import VectorStoreIndex, Document
+from llama_index import VectorStoreIndex, StorageContext, load_index_from_storage
 
 logging.basicConfig(level=logging.INFO)
 
@@ -21,12 +21,17 @@ def main():
     git_repo_path = Path(os.getenv("GIT_REPO_PATH"))
     docs_path = git_repo_path / "docs"
 
-    documents = process_markdown_files(docs_path)
-
-    index = VectorStoreIndex.from_documents(documents)
-
     # Clone or update the repository
     clone_or_pull_repository(git_repo_url, git_repo_path)
+
+    # Load or initialize the index
+    try:
+        storage_context = StorageContext.from_defaults(persist_dir="./storage")
+        index = load_index_from_storage(storage_context)
+    except:
+        # Create an index from the documents if it doesn't exist
+        documents = process_markdown_files(docs_path)
+        index = VectorStoreIndex.from_documents(documents)
 
     # Get all markdown files
     markdown_files = get_markdown_files(git_repo_path)
@@ -41,16 +46,13 @@ def main():
         # Initialize MarkdownReader
         markdown_reader = MarkdownReader()
 
-        # Process the updated markdown files
-        for file in changed_files:
-            extra_info = {"original_file_path": str(file)}
-            new_documents = markdown_reader.load_data(file, extra_info=extra_info)
-            
-            # Insert new header-docs into the index
-            for doc in new_documents:
-                index.insert(Document(text=doc.text, metadata=doc.metadata))
+        # Update the index for changed files
+        update_index_for_changed_files(index, changed_files, markdown_reader)
     else:
         print("No changes detected.")
+
+    # Persist the index
+    index.storage_context.persist()
 
 if __name__ == "__main__":
     main()
