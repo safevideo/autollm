e05e42ca12bdc5937ee554fd914f3c89b75975ab
SeeknnDestroy <sarit17@itu.edu.tr>
2023-10-04 17:24:36 +0300

    Add configurations

--- ai_doc_assistant_setup.py
+++ ai_doc_assistant_setup.py
@@ -1,19 +1,41 @@
-# First, let's import the necessary modules and utility functions
+import logging
+
 from typing import Tuple
+from dotenv import load_dotenv
+from os.path import dirname, join
+import os
+
 from llama_index import (
     VectorStoreIndex,
-    PromptTemplate,
+    PromptHelper,
     ServiceContext,
     set_global_service_context
 )
+from llama_index.node_parser import SimpleNodeParser
+from llama_index.prompts import ChatPromptTemplate, ChatMessage, MessageRole
 from llama_index.query_engine import CustomQueryEngine
 from llama_index.retrievers import BaseRetriever
 from llama_index.response_synthesizers import get_response_synthesizer, BaseSynthesizer
+from llama_index.text_splitter import TokenTextSplitter
+
 from llama_utils import initialize_or_load_index
-import logging
+
 
 # Initialize logger
 logger = logging.getLogger(__name__)
+
+def read_env_variable(variable_name: str, default_value: str = None) -> str:
+    """Reads an environment variable, returning a default value if not found."""
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    return os.getenv(variable_name, default_value)
+
+
+max_tokens = int(read_env_variable("MAX_TOKENS", 1024))
+chunk_size = int(read_env_variable("CHUNK_SIZE", 1024))
+chunk_overlap = int(read_env_variable("CHUNK_OVERLAP", 20))
+context_window = int(read_env_variable("CONTEXT_WINDOW", 4096))
+similarity_top_k = int(read_env_variable("SIMILARITY_TOP_K", 4))
 
 # Define the system prompt and query prompt template
 system_prompt = '''
@@ -31,6 +53,39 @@ The document information is below.
 ---------------------
 Using the document information and your capabilities, answer the following query: {query_str}
 '''
+
+# Text QA Prompt
+chat_text_qa_msgs = [
+    ChatMessage(
+        role=MessageRole.SYSTEM,
+        content=system_prompt,
+    ),
+    ChatMessage(
+        role=MessageRole.USER,
+        content=query_prompt_template,
+    ),
+]
+text_qa_template = ChatPromptTemplate(chat_text_qa_msgs)
+
+# Initialize LLM based on the backend selection
+llm = initialize_llm()
+
+node_parser = SimpleNodeParser.from_defaults(
+    text_splitter=TokenTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+)
+prompt_helper = PromptHelper(
+    context_window=context_window,
+    num_output=256,
+    chunk_overlap_ratio=0.1,
+    chunk_size_limit=None
+)
+
+service_context = ServiceContext.from_defaults(
+    llm=llm,
+    node_parser=node_parser,
+    prompt_helper=prompt_helper,
+)
+set_global_service_context(service_context)
 
 # Create a custom query engine class
 class AIDocQueryEngine(CustomQueryEngine):
