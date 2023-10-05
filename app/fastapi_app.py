39802aab04335066d2795ce6435393e2271ec77a
SeeknnDestroy <sarit17@itu.edu.tr>
2023-10-05 16:26:54 +0300

    update fastapi

--- app/fastapi_app.py
+++ app/fastapi_app.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI
-from pathlib import Path
 import logging
 
-from ai_doc_assistant_setup import initialize_query_engine, initialize_service_context
-from git_utils import clone_or_pull_repository
-from env_utils import read_env_variable
+from fastapi import FastAPI, HTTPException
+from fastapi.logger import logger
+
+from ai_doc_assistant_setup import initialize_service_context
+from llama_utils import connect_database
 from fastapi_docs import (
     title,
     description,
@@ -14,7 +14,8 @@ from fastapi_docs import (
     tags_metadata,
 )
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(handlers=[logging.StreamHandler()],
+                    level=logging.INFO)
 
 # Initialize FastAPI and Logging
 app = FastAPI(
@@ -26,41 +27,49 @@ app = FastAPI(
     openapi_tags=tags_metadata,
 )
 
-# Get environment variables
-git_repo_url = read_env_variable("GIT_REPO_URL")
-git_repo_path = Path(read_env_variable("GIT_REPO_PATH"))
-# Configure where the markdown files are located
-docs_path = git_repo_path / "docs"
-
-# Clone or pull the git repository to get the latest markdown files
-clone_or_pull_repository(git_repo_url, git_repo_path)
-
 # Initialize the service context
 service_context = initialize_service_context()
 
 # Initialize the query engine
-query_engine = initialize_query_engine(docs_path=docs_path)
+try:
+    index = connect_database()
+    query_engine = index.as_query_engine(service_context=service_context)
+except Exception as e:
+    logger.error(f"Failed to connect to database: {e}")
+    raise HTTPException(status_code=500, detail="Database connection failed")
 
 @app.get("/ask_question", tags=["ask"])
 async def ask_question(user_query: str):
     """
-    Perform Text-Based Queries on Document Store
-    
-    This endpoint receives a natural language query from the user and returns the most relevant answer from the document store.
+    Performs text-based queries on the document store.
 
-    Args:
+    This endpoint receives a natural language query from the user and fetches the most relevant answer from the document store.
+
+    Parameters:
         user_query (str): The natural language query from the user.
 
     Returns:
-        dict: The response containing the answer to the user's query.
+        dict: The response containing the most relevant answer to the user's query.
     """
-    # Query the engine
-    response = query_engine.query(user_query)
-    return response
+    try:
+        # Execute the query on the engine
+        response = query_engine.query(user_query)
+        return response
+    except Exception as e:
+        logger.error(f"Query execution failed: {e}")
+        raise HTTPException(status_code=500, detail="Query execution failed")
+
 
 @app.get("/health", tags=["health"])
 async def health_check():
     """
-    Health check endpoint.
+    Checks the health of the service and its dependencies.
+    
+    Returns:
+        dict: The health status of the service.
     """
-    return {"status": "healthy"}
+    try:
+        return {"status": "healthy"}
+    except Exception as e:
+        logger.error(f"Health check failed: {e}")
+        return {"status": "unhealthy"}
