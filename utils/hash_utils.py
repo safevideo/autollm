772467881a33a3df42160161ba9c999c846059f3
SeeknnDestroy <sarit17@itu.edu.tr>
2023-10-08 20:51:32 +0300

    What am I doing???

--- utils/hash_utils.py
+++ utils/hash_utils.py
@@ -1,9 +1,11 @@
 import hashlib
 import logging
 from pathlib import Path
-from typing import Dict, Sequence
+from typing import Dict, Sequence, List, Tuple
 
 from llama_index.schema import Document
+
+from vectorstores.base import BaseVS
 
 logger = logging.getLogger(__name__)
 
@@ -25,66 +27,57 @@ def get_md5(file_path: Path) -> str:
     return hasher.hexdigest()
 
 
-def load_last_hashes(hash_file: Path) -> Dict[str, str]:
-    """
-    Load the last known hashes from a file.
-
-    Parameters:
-        hash_file (Path): The path to the hash file.
-
-    Returns:
-        Dict[str, str]: A dictionary mapping file paths to their MD5 hashes.
-    """
-    if hash_file.exists():
-        with open(hash_file, 'r') as f:
-            return {line.split()[0]: line.split()[1] for line in f.readlines()}
-    return {}
-
-
-def save_current_hashes(current_hashes: Dict[str, str], hash_file: Path) -> None:
-    """
-    Save the current hashes to a file.
-
-    Parameters:
-        current_hashes (Dict[str, str]): A dictionary mapping file paths to their MD5 hashes.
-        hash_file (Path): The path to the hash file.
-    """
-    with open(hash_file, 'w') as f:
-        for file, hash in current_hashes.items():
-            f.write(f"{file} {hash}\n")
-
-
 # TODO: check md5 hashes from vector store index metadata instead of a local txt file (3)
-def check_for_changes(documents: Sequence[Document], hash_file: Path = Path("file_hashes.txt")) -> Sequence[Document]:
+def check_for_changes(documents: Sequence[Document], vs: BaseVS) -> Tuple[Sequence[Document], List[str]]:
     """
-    Check for file changes based on their MD5 hashes.
+    Check for file changes based on their hashes.
 
     Parameters:
         documents (List[Document]): List of documents to check for changes.
-        hash_file (Path): The path to the hash file.
-
+        vs (BaseVS): The vector store to check for changes in.
+    
     Returns:
         List[Document]: List of documents that have changed.
+
+        List[str]: List of document ids that are deleted in local but present in vectore store.
     """
-    last_hashes = load_last_hashes(hash_file)
+    last_hashes, original_file_names, document_ids = vs.get_document_infos()
     current_hashes = {}
     changed_files = []
+    deleted_document_ids = []
 
     markdown_files = [Path(doc.metadata["original_file_path"]) for doc in documents]
 
     for file in markdown_files:
         current_hash = get_md5(file)
         current_hashes[str(file)] = current_hash
-        if str(file) not in last_hashes or last_hashes[str(file)] != current_hash:
+        # Add
+        if str(file) not in last_hashes:
             changed_files.append(file)
+        # Update
+        elif last_hashes[str(file)] != current_hash:
+            changed_files.append(file)
+        # Remove (file was deleted)
+        elif str(file) in last_hashes:
+            deleted_document_ids.append(file)
 
     logger.info(f"Found {len(changed_files)} changed files.")
-
-    save_current_hashes(current_hashes, hash_file)
+    logger.info(f"Found {len(deleted_document_ids)} deleted documents.")
 
     changed_documents = []
     for doc in documents:
         if Path(doc.metadata["original_file_path"]) in changed_files:
             changed_documents.append(doc)
 
-    return changed_documents
+    return changed_documents, deleted_document_ids
+
+
+def check_for_changes(documents: Sequence[Document], vs: BaseVS) -> Tuple[Sequence[Document], List[str]]:
+    """
+    Check for file changes based on their hashes.
+
+    Returns:
+        List[Document]: List of documents that have changed.
+
+        List[str]: List of document ids that are deleted in local but present in vectore store.
+    """
