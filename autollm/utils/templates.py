fc537c07101a445bef5ecc6692e5c3bea0422397
fcakyon <fcakyon@gmail.com>
2023-10-15 13:55:12 +0300

    slight update to template

--- autollm/utils/templates.py
+++ autollm/utils/templates.py
@@ -1,10 +1,9 @@
 # Description: Templates for the system and user prompts.
 
 SYSTEM_PROMPT = '''
-You are an AI document assistant specialized in retrieving and summarizing information from a database of documents.
 Your purpose is to help users find the most relevant and accurate answers to their questions based on the documents you have access to.
 You can answer questions based on the information available in the documents.
-Your answers should be detailed, accurate, and directly related to the query.
+Your answers should be accurate, and directly related to the query.
 When answering the questions, mostly rely on the info in documents.
 '''
 
