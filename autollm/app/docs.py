e1396325fb3da4c59214a877a807fd36147021f9
SeeknnDestroy <sarit17@itu.edu.tr>
2023-10-18 13:08:38 +0300

    update fastapi docs

--- autollm/app/docs.py
+++ autollm/app/docs.py
@@ -1,20 +1,16 @@
 # Purpose: FastAPI documentation configuration.
 # Metadata
-title = "SafeVideo Query Engine"
+title = "AutoLLM Query Engine"
 description = """
-This is a FastAPI service for SafeVideo's natural language query engine.
-It's designed to query multiple and big Markdown documents and get the most relevant results.
+This is a FastAPI service for AutoLLM's natural language query engine.
+It's designed to query multiple and big documents and get the most relevant results.
 """
-version = "1.0.0"
+version = "0.0.1"
 openapi_url = "/api/v1/openapi.json"
 terms_of_service = "Local Deployment, All Rights Reserved."
 tags_metadata = [
     {
-        "name": "ask",
+        "name": "query",
         "description": "Operations related to querying the header-documents."
-    },
-    {
-        "name": "health",
-        "description": "Health check operations."
     },
 ]
