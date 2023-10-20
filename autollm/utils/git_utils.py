4423787154e07807096056295a9fa456a209aa65
SeeknnDestroy <sarit17@itu.edu.tr>
2023-10-20 18:59:18 +0300

    lazy import gitpython

--- autollm/utils/git_utils.py
+++ autollm/utils/git_utils.py
@@ -1,8 +1,6 @@
 import logging
 from pathlib import Path
 
-from git import Repo
-
 logger = logging.getLogger(__name__)
 
 
@@ -14,6 +12,14 @@ def clone_or_pull_repository(git_url: str, local_path: Path) -> None:
         git_url (str): The URL of the Git repository.
         local_path (Path): The local path where the repository will be cloned or updated.
     """
+    # Lazy import to avoid dependency on GitPython
+    try:
+        from git import Repo
+    except ImportError:
+        logger.error(
+            'GitPython is not installed. Please "pip install gitpython==3.1.37" to use this feature.')
+        raise
+
     if local_path.exists():
         logger.info(f'Updating existing repository at {local_path}')
         repo = Repo(str(local_path))
