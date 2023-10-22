9947eff081cf691190ae513ade1e0db4e7267dfb
Talha SARI <sarit17@itu.edu.tr>
2023-10-22 14:12:31 +0300

    update version to 0.0.6 (#28)

--- autollm/__init__.py
+++ autollm/__init__.py
@@ -4,7 +4,7 @@ This package provides automated integrations with leading large language models
 and vector databases, along with various utility functions.
 """
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 __author__ = 'safevideo'
 __license__ = 'AGPL-3.0'
 
