74117fc44b8bbcee83c357b8281dba6d02832feb
SeeknnDestroy <sarit17@itu.edu.tr>
2023-11-04 22:49:14 +0300

    fix bugs on web page reader

--- autollm/utils/web_page_reader.py
+++ autollm/utils/web_page_reader.py
@@ -1,11 +1,11 @@
+import logging
 from typing import List
-from urllib.parse import urlparse
 
 import requests
 from bs4 import BeautifulSoup
 from llama_index.schema import Document
 
-# Selectors to identify the main content of the page
+# Constants defined outside the class
 SELECTORS = [
     "article.bd-article",
     'article[role="main"]',
@@ -17,7 +17,6 @@ SELECTORS = [
     "main",
 ]
 
-# Tags to ignore while extracting content
 IGNORED_TAGS = [
     "nav",
     "aside",
@@ -31,41 +30,48 @@ IGNORED_TAGS = [
     "style",
 ]
 
+logger = logging.getLogger(__name__)
+
 
 class WebPageReader:
-    """Class to read and process content from a single web page."""
+    """A class for reading and processing the content of a single web page."""
 
     def load_data(self, url: str) -> List[Document]:
         """
-        Load and process content from the given URL.
+        Reads a web page from the provided URL, extracts content using predefined selectors, removes all
+        ignored tags, and returns a list of Document objects with the processed content.
 
-        Args:
-            url (str): The URL of the web page to process.
+        Parameters:
+            url (str): The URL of the web page to read.
 
         Returns:
-            List[Document]: A list containing a single Document object with the processed content.
+            List[Document]: A list containing Document objects with the processed content and its metadata.
         """
         response = requests.get(url)
         if response.status_code != 200:
-            raise ValueError(f"Failed to fetch the website: {response.status_code}")
+            logger.info(f"Failed to fetch the website: {response.status_code}")
+            return []
 
         soup = BeautifulSoup(response.content, "html.parser")
 
-        content = None
         for selector in SELECTORS:
             element = soup.select_one(selector)
             if element:
                 content = element.prettify()
                 break
-        if content is None:
+        else:
+            logger.info(f"Failed to find any element for URL: {url}")
             content = soup.get_text()
 
-        # Clean the content by removing ignored tags
         soup = BeautifulSoup(content, "html.parser")
         for tag in soup(IGNORED_TAGS):
             tag.decompose()
 
-        content_cleaned = " ".join(soup.stripped_strings)
-        document = Document(text=content_cleaned, metadata={"url": url})
-
+        content = " ".join(soup.stripped_strings)
+        document = Document(text=content, metadata={"url": url})
         return [document]
+
+
+# Example of usage:
+# reader = WebPageReader()
+# documents = reader.load_data('http://example.com/some-page')
