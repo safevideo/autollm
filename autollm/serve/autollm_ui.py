8986bf748befc3c870fbc6e0250b3cc413dd8989
SeeknnDestroy <sarit17@itu.edu.tr>
2023-11-10 18:38:53 +0300

    refactor process input into seperate functions

--- autollm/serve/autollm_ui.py
+++ autollm/serve/autollm_ui.py
@@ -3,9 +3,22 @@ import os
 import gradio as gr
 
 
-def process_input(web_page_field, directory_field, document_field):
-    # Placeholder function to process the input
-    return f"Web Page Field: {web_page_field}, Directory Field: {directory_field}, Document Field: {document_field}"
+# Function to process web page field input
+def process_web_page_field(web_page_field):
+    # Placeholder logic for processing web page field input
+    return f"Processed Web Page Field: {web_page_field}"
+
+
+# Function to process directory field input
+def process_directory_field(directory_field):
+    # Placeholder logic for processing directory field input
+    return f"Processed Directory Field: {directory_field}"
+
+
+# Function to process document field input
+def process_document_field(document_field):
+    # Placeholder logic for processing document field input
+    return f"Processed Document Field: {document_field}"
 
 
 def load_config():
@@ -35,7 +48,12 @@ with gr.Blocks() as app:
     load_config_btn.click(load_config, [], output)
     load_env_btn.click(load_env, [], output)
 
-    submit_btn = gr.Button("Submit")
-    submit_btn.click(process_input, [web_page_field, directory_field, document_field], output)
+    submit_web_page_btn = gr.Button("Submit Web Page Field")
+    submit_directory_btn = gr.Button("Submit Directory Field")
+    submit_document_btn = gr.Button("Submit Document Field")
+
+    submit_web_page_btn.click(process_web_page_field, [web_page_field], output)
+    submit_directory_btn.click(process_directory_field, [directory_field], output)
+    submit_document_btn.click(process_document_field, [document_field], output)
 
 app.launch()
