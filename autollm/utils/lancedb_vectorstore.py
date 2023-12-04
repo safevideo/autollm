d84098b294e95f5365eabebab236a713d1c5e7a7
SeeknnDestroy <sarit17@itu.edu.tr>
2023-12-04 17:52:48 +0300

    add enviorenment variable check for api key

--- autollm/utils/lancedb_vectorstore.py
+++ autollm/utils/lancedb_vectorstore.py
@@ -1,7 +1,11 @@
 """LanceDB vector store with cloud storage support."""
+import os
 from typing import Any, Optional
 
+from dotenv import load_dotenv
 from llama_index.vector_stores import LanceDBVectorStore as LanceDBVectorStoreBase
+
+load_dotenv()
 
 
 class LanceDBVectorStore(LanceDBVectorStoreBase):
@@ -21,6 +25,12 @@ class LanceDBVectorStore(LanceDBVectorStoreBase):
             import lancedb
         except ImportError:
             raise ImportError(import_err_msg)
+
+        # Check for API key and region in environment variables if not provided
+        if api_key is None:
+            api_key = os.getenv('LANCEDB_API_KEY')
+        if region is None:
+            region = os.getenv('LANCEDB_REGION')
 
         if api_key and region:
             self.connection = lancedb.connect(uri, api_key=api_key, region=region)
