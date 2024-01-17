1497a1959f44bf0f052b5553d3d88208ee59a843
SeeknnDestroy <sarit17@itu.edu.tr>
2024-01-17 19:25:25 +0300

    implement export page

--- autollm/serve/cli.py
+++ autollm/serve/cli.py
@@ -1,17 +1,47 @@
 import gradio as gr
 
 
-def start_gradio_app():
+def create_preview(hf_api_key, make_db_private):
+    # Placeholder for the logic to create the preview.
+    preview_content = f"API Key: , DB Private: {make_db_private}"
+    return preview_content
 
-    def dummy_function(input_text):
-        return "Dummy Response for: " + input_text
 
-    iface = gr.Interface(fn=dummy_function, inputs="text", outputs="text")
-    iface.launch()
+def submit_message(message):
+    # Placeholder for the logic to process the message.
+    return f"Message received: {message}"
+
+
+with gr.Blocks() as demo:
+    gr.Markdown("### AutoLLM UI")  # Title for the app
+    with gr.Row():
+        with gr.Column():
+            with gr.Tab("Create"):
+                # Controls for 'Create' tab
+                pass
+            with gr.Tab("Configure"):
+                # Controls for 'Configure' tab
+                pass
+            with gr.Tab("Export"):
+                # Controls for 'Export' tab
+                hf_api_key = gr.Textbox(label="Hf api key:", type="password")
+                make_db_private = gr.Checkbox(label="Make db private")
+                create_preview_button = gr.Button("Create Preview")
+        with gr.Column():
+            preview_area = gr.Textbox(label="Preview", interactive=False)
+            message_input = gr.Textbox(label="Message llm...")
+            submit_button = gr.Button("Submit")
+            download_api_button = gr.Button("Download API")
+            deploy_button = gr.Button("Deploy to 🚀")
+
+    # Define interactions
+    create_preview_button.click(create_preview, inputs=[hf_api_key, make_db_private], outputs=[preview_area])
+
+    submit_button.click(submit_message, inputs=[message_input], outputs=[preview_area])
 
 
 def main():
-    start_gradio_app()
+    demo.launch()
 
 
 if __name__ == "__main__":
