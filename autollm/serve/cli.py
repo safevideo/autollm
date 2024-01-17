54b25820a00650b37285b689d41e14c333fa4e09
SeeknnDestroy <sarit17@itu.edu.tr>
2024-01-17 20:28:20 +0300

    implement configure tab ui

--- autollm/serve/cli.py
+++ autollm/serve/cli.py
@@ -15,6 +15,12 @@ def submit_message(message):
     return f"Message received: {message}"
 
 
+def configure_app(config_file, emoji, name, description, instruction):
+    # Here you would process the configuration file and inputs.
+    # Placeholder function for the sake of example.
+    return "Configuration updated (dummy response)."
+
+
 with gr.Blocks() as demo:
     gr.Markdown("### AutoLLM UI")  # Title for the app
     with gr.Row():
@@ -25,13 +31,30 @@ with gr.Blocks() as demo:
                 with gr.Tab("Palm"):
                     # Controls for 'Palm' tab
                     pass
-                file_upload = gr.File(label="Add knowledge from files", file_count="multiple")
-                webpage_input = gr.Textbox(label="Add knowledge from webpages")
-                what_to_make_area = gr.Textbox(label="What would you like to make")
+
+                with gr.Column(variant="compact"):
+                    file_upload = gr.File(label="Add knowledge from files", file_count="multiple")
+                    webpage_input = gr.Textbox(label="Add knowledge from webpages")
+                what_to_make_area = gr.Textbox(label="What would you like to make?")
                 create_preview_button = gr.Button("Create Preview")
+
             with gr.Tab("Configure"):
-                # Controls for 'Configure' tab
-                pass
+                with gr.Column(variant="compact"):
+                    detail_html = gr.HTML(
+                        '<a href="https://github.com/safevideo/autollm/blob/main/examples/configs/config.example.yaml">click here for example config</a>'
+                    )
+                    config_file_upload = gr.File(label="Load .config file", file_count="single")
+                    emoji_input = gr.Textbox(label="emoji:")
+                    name_input = gr.Textbox(label="name:")
+                    description_input = gr.TextArea(label="description:")
+                    instruction_input = gr.TextArea(label="instruction:")
+                    configure_button = gr.Button("Create Preview")
+                    configure_button.click(
+                        configure_app,
+                        inputs=[
+                            config_file_upload, emoji_input, name_input, description_input, instruction_input
+                        ],
+                        outputs=[])
             with gr.Tab("Export"):
                 # Controls for 'Export' tab
                 hf_api_key = gr.Textbox(label="Hf api key:", type="password")
@@ -55,7 +78,6 @@ with gr.Blocks() as demo:
             msg.submit(respond, [msg, chatbot], [msg, chatbot])
 
     # Define interactions
-    # create_preview_button.click(create_preview, inputs=[hf_api_key, make_db_private], outputs=[preview_area])
 
 
 def main():
