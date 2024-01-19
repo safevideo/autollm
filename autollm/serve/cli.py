0855bd57473c85ed691dad579133463bc871e385
SeeknnDestroy <sarit17@itu.edu.tr>
2024-01-19 21:42:30 +0300

    major updates on cli module

--- autollm/serve/cli.py
+++ autollm/serve/cli.py
@@ -6,17 +6,13 @@ from llama_index import Document
 
 from autollm.auto.llm import AutoLiteLLM
 from autollm.auto.query_engine import AutoQueryEngine
-from autollm.serve.prompts import LLM_BUILDER_SYSTEM_PROMPT
+from autollm.serve.llm_utils import create_custom_llm
 from autollm.utils.document_reading import read_files_as_documents
 
 llama_index.set_global_handler("simple")
 
-llm_builder = AutoLiteLLM.from_defaults(system_prompt=LLM_BUILDER_SYSTEM_PROMPT)
 
-
-def configure_app(
-        openai_api_key, palm_api_key, uploaded_files, webpage_input, what_to_make_area, config_file, emoji,
-        name, description, instruction):
+def create_app(openai_api_key, palm_api_key, what_to_make_area, uploaded_files, webpage_input, config_file):
     global query_engine
     progress = gr.Progress()
 
@@ -25,6 +21,10 @@ def configure_app(
 
     progress(0.2, desc="Reading files...")
     file_documents = read_files_as_documents(input_files=uploaded_files)
+
+    progress(0.4, desc="Updating LLM...")
+    custom_llm = create_custom_llm(user_prompt=what_to_make_area, config=config_file)
+    emoji, name, description, instruction = update_configurations(custom_llm)
 
     progress(0.8, desc="Configuring app..")
     query_engine = AutoQueryEngine.from_defaults(
@@ -36,8 +36,22 @@ def configure_app(
 
     # Complete progress
     progress(1.0, desc="Completed")  # Complete progress bar
+    create_preview_output = gr.Textbox("App preview created on the right screen.")
 
-    return gr.Textbox("App preview created on the right screen.")
+    return create_preview_output, emoji, name, description, instruction
+
+
+def update_configurations(custom_llm):
+    emoji = custom_llm.emoji
+    name = custom_llm.name
+    description = custom_llm.description
+    instruction = custom_llm.instructions
+
+    return gr.Textbox(emoji), gr.Textbox(name), gr.Textbox(description), gr.Textbox(instruction)
+
+
+def update_app():
+    pass
 
 
 def predict(message, history):
@@ -89,15 +103,15 @@ with gr.Blocks(title="autollm UI", theme=gr.themes.Default(primary_hue=gr.themes
                     with gr.Accordion(label="Load config file", open=False):
                         config_file_upload = gr.File(
                             label="Configurations of LLM, Vector Store..", file_count="single")
-                    emoji_input = gr.Textbox(label="Emoji")
-                    name_input = gr.Textbox(label="Name")
-                    description_input = gr.Textbox(label="Description")
-                    instruction_input = gr.TextArea(label="Instructions")
+                    emoji = gr.Textbox(label="Emoji")
+                    name = gr.Textbox(label="Name")
+                    description = gr.Textbox(label="Description")
+                    instruction = gr.TextArea(label="Instructions")
                     with gr.Row():
                         with gr.Column(scale=1, min_width=10):
                             placeholder = gr.Button(visible=False, interactive=False)
                         with gr.Column(scale=1, min_width=100):
-                            create_preview_button_2 = gr.Button("Create Preview", variant="primary")
+                            update_preview_button = gr.Button("Update Preview", variant="primary")
                 configure_output = gr.Textbox(label="👆 Click `Create Preview` to see preview of the LLM app")
             with gr.Tab("Export"):
                 # Controls for 'Export' tab
@@ -121,18 +135,18 @@ with gr.Blocks(title="autollm UI", theme=gr.themes.Default(primary_hue=gr.themes
                     chat_interface = gr.ChatInterface(predict, chatbot=chatbot)
 
         create_preview_button.click(
-            configure_app,
+            create_app,
             inputs=[
-                openai_api_key_input, palm_api_key_input, uploaded_files, webpage_input, what_to_make_area,
-                config_file_upload, emoji_input, name_input, description_input, instruction_input
+                openai_api_key_input, palm_api_key_input, what_to_make_area, uploaded_files, webpage_input,
+                config_file_upload
             ],
-            outputs=[create_preview_output])
+            outputs=[create_preview_output, emoji, name, description, instruction])
 
-        create_preview_button_2.click(
-            configure_app,
+        update_preview_button.click(
+            update_app,
             inputs=[
-                openai_api_key_input, palm_api_key_input, uploaded_files, webpage_input, what_to_make_area,
-                config_file_upload, emoji_input, name_input, description_input, instruction_input
+                openai_api_key_input, palm_api_key_input, what_to_make_area, uploaded_files, webpage_input,
+                config_file_upload, emoji, name, description, instruction
             ],
             outputs=[configure_output],
             scroll_to_output=True)
