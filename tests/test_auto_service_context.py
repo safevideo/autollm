660250a7f72aae2848d2cc78ed383b4af4b9ab84
SeeknnDestroy <sarit17@itu.edu.tr>
2024-01-23 20:27:26 +0300

    update tests for autoservicecontext with azure

--- tests/test_auto_service_context.py
+++ tests/test_auto_service_context.py
@@ -1,12 +1,35 @@
+import os
+
 from llama_index import Document, ServiceContext, VectorStoreIndex
+from llama_index.embeddings import AzureOpenAIEmbedding
+from llama_index.llms import AzureOpenAI
 
 from autollm.auto.service_context import AutoServiceContext
+
+# set the environment variables
+azure_api_key = os.environ.get("AZURE_API_KEY")
+azure_endpoint = os.environ.get("AZURE_API_BASE")
+azure_api_version = os.environ.get("AZURE_API_VERSION")
 
 
 def test_auto_service_context():
     document = Document.example()
-
-    service_context = AutoServiceContext.from_defaults(enable_cost_calculator=True)
+    llm = AzureOpenAI(
+        engine="gpt-35-turbo-1106",
+        model="gpt-35-turbo-16k",
+        api_key=azure_api_key,
+        azure_endpoint=azure_endpoint,
+        api_version=azure_api_version,
+    )
+    embed_model = AzureOpenAIEmbedding(
+        model="text-embedding-ada-002",
+        deployment_name="text-embedding-ada-002",
+        api_key=azure_api_key,
+        azure_endpoint=azure_endpoint,
+        api_version=azure_api_version,
+    )
+    service_context = AutoServiceContext.from_defaults(
+        enable_cost_calculator=True, llm=llm, embed_model=embed_model)
 
     # Check if the service_context is an instance of ServiceContext
     assert isinstance(service_context, ServiceContext)
